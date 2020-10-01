2d9b772ab2ffe33accffef783c07ed5211cbce30
leila571 <leilamarks@postoakschool.org>
2020-10-01 12:22:26 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+'''
+10/1/2020
+Leila Lopez Marks
+Tic Tac Toe Board
+'''
+
+print("Welcome to Tic Tac Toe")
+board = [None, None, "None", "None", "None", None, "None", None, "None"]
+
+
+# python ttt.py
+[['_', '_', '_'], ['_', '_', '_'], ['_', '_', '_']]
+
+# ttt.py
+
+board = [
+    ["_","_", "_"],
+    ["_", "_", "_"],
+    ["_", "_", "_"]
+]
+
+for row in board:
+    print(row)
+
+# python ttt.py
+['_', '_', '_']
+['_', '_', '_']
+['_', '_', '_']
